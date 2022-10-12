dccf042e3b1acc4154678ed0bfa218aa171ee411
DevTinel <davidaugustolol36@gmail.com>
2022-10-12 15:24:31 -0300

    validacoes

--- index.js
+++ index.js
@@ -6,27 +6,33 @@ const small = document.querySelector('small');
 const isValidBRZip = (cep) => /^[0-9]{5}-[0-9]{3}$/.test(cep);
 async function buscar() {
   const cep = document.querySelector('input').value;
+  const resposta = fetch(`https://viacep.com.br/ws/${cep}/json`);
+  const body = await (await resposta).json();
   if (isValidBRZip(cep)) {
-    const resposta = fetch(`https://viacep.com.br/ws/${cep}/json`);
-    const body = await (await resposta).json();
-
-    input.classList.remove('err');
-    small.classList.add('hidden');
-    console.log(body);
-    resultado.innerHTML = `
-    <div class = "inf" >
-    <p><span>Cep</span>  : ${body.cep}</p>
-    <p><span>Logradouro</span>: ${body.logradouro}</p>
-    <p><span>Bairro</span>: ${body.bairro}</p>
-    <p><span>Localidade:</span>  ${body.localidade}</p>
-    <p><span>UF:</span> ${body.uf}</p>
-    <p><span>ddd:</span>${body.ddd}</p>
-    </div>
-    `;
-  } else {
-    input.classList.add('err');
-    small.classList.remove('hidden');
-    resultado.innerHTML = '';
+    if (body.erro) {
+      input.classList.add('err');
+      small.classList.remove('hidden');
+      resultado.innerHTML = '';
+    } else if (body) {
+      console.log(body);
+      input.classList.remove('err');
+      small.classList.add('hidden');
+      console.log(body);
+      resultado.innerHTML = `
+      <div class = "inf" >
+      <p><span>Cep</span>  : ${body.cep}</p>
+      <p><span>Logradouro</span>: ${body.logradouro}</p>
+      <p><span>Bairro</span>: ${body.bairro}</p>
+      <p><span>Localidade:</span>  ${body.localidade}</p>
+      <p><span>UF:</span> ${body.uf}</p>
+      <p><span>ddd:</span>${body.ddd}</p>
+      </div>
+      `;
+    } else {
+      input.classList.add('err');
+      small.classList.remove('hidden');
+      resultado.innerHTML = '';
+    }
   }
 }
 button.addEventListener('click', buscar);
